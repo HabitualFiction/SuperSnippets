bf0eb788e76cf91d8f05a0a7ca4ea47de75a99ee
Jake Sankari <jakesankari@gmail.com>
2018-07-23 10:21:53 -0400

    Initial Commit
    drivefailure.sh command store

---
+++ Terminal/HCOS_Commands/driveFailure.sh
@@ -0,0 +1,28 @@
+###Condition Show
+
+#Shows all currently set conditions
+sc condition show display set
+
+#Shows all conditions possible
+sc condition show 
+
+
+
+###Drive & Node Data
+
+#Show drive condition and informatoin
+sc drive show
+
+#Show node condition and information
+sc node show
+
+
+
+###Logs
+
+#Messages
+less /var/log/Messages
+
+#scaled
+less /var/scale/scaled.log
+

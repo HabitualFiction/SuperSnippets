42f0dbb9e0ae2c8fa07edea551059796610d214c
Jake Sankari <jakesankari@gmail.com>
2018-07-25 10:42:34 -0400

    New CentOS file
    Added helpful CentOS file that currently only has information on how to handle networking on a CentOS VM

---
+++ Terminal/CentOS.sh
@@ -0,0 +1,7 @@
+###DHCP Files
+# /etc/sysconfig/network
+NETWORKING=yes
+# /etc/sysconfig/network-scripts/ifcfg-eth0
+BOOTPROTO=dhcp
+DEVICE=eth0
+ONBOOT=yes
--- Terminal/HCOS_Commands/basicCommands.sh
+++ Terminal/HCOS_Commands/basicCommands.sh
@@ -1,2 +1,5 @@
 #Complete command on all nodes on a cluster
 scdoall
+
+#Show cluster log - same information on the UI - You can use
+sc clusterlog show
